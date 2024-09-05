b52e24583a3bdb17ed80436d8ba9a8cec3168e6d
alexpinch <alexpinch@hotmail.com>
2024-09-05 11:05:17 -0700

    Update rarefaction.R

--- temp/rarefaction.R
+++ temp/rarefaction.R
@@ -12,14 +12,23 @@
 
 # tidyverse is for making plots
 library(tidyverse)
+library(vegan)
 
 # Set working directory to where you've downloaded the specimen catalogue as a .csv
 setwd("/Users/alexpinch/Documents/obs_vault/Gorge_work_2024/gorge_pollinators_2024/data")
 
 # loading and filtering the data, selecting the columns they have in common and binding them together
-pan_trap_data <- read.csv("20240729_pantraps_2024.csv")
-visitation_sampling_data <- read.csv("20240729_visitation_2024.csv")
-# Identify common columns and combine data frames
+pan_trap_data_2023 <- read.csv("20240729_pantraps_2023.csv")
+visitation_sampling_data_2023 <- read.csv("20240729_visitation_2024.csv")
+pan_trap_data_2024 <- read.csv("20240729_pantraps_2024.csv")
+visitation_sampling_data_2024 <- read.csv("20240729_visitation_2024.csv")
+
+# this might be a one-off, for some reason pan trap data for 2023 the date is formatted differently. changing to match the others
+pan_trap_data_2023$Collection.Date <- format(as.Date(pan_trap_data_2023$Collection.Date, format = "%B %d, %Y"), "%d-%b-%y")
+
+pan_trap_data <- rbind(pan_trap_data_2023, pan_trap_data_2024)
+visitation_sampling_data <- rbind(visitation_sampling_data_2023, visitation_sampling_data_2024)
+
 common_columns <- intersect(colnames(pan_trap_data), colnames(visitation_sampling_data))
 data <- rbind(
   pan_trap_data %>% select(all_of(common_columns)),
@@ -30,37 +39,35 @@ data <- data %>%
   filter(!grepl("Non-survey", Comments)) %>% # removes non-survey
   filter(Family != "") # removes individuals not identified to family
 
-# empty dataframe to store the results
-average_genera <- data.frame(Average = numeric(0))
+data$Collection.Date <- as.Date(data$Collection.Date, format = "%d-%b-%y")
 
-for (i in 1:nrow(data)) {
-
-  random_rows <- data[sample(1:nrow(data), 10, replace = TRUE), ]
-  unique_strings <- length(unique(random_rows$Genus))
-  average_unique_strings <- unique_strings / 10
- 
-  # Making sure this is working before adding to empty dataframe 
-  # print("Unique strings:")
-  # cat(unique_strings)
-  # print("average unique strings:")
-  # cat(average_unique_strings)
-  # Sys.sleep(2)
-  
-  average_genera <- rbind(average_genera, data.frame(Average = average_unique_strings))
-}
-
-# View the resulting dataframe
-print(average_genera)
-
-plot1 <- average_genera %>%
-  ggplot(aes(x = 1:nrow(data), y = Average)) +
+plot1 <- data %>%
+  group_by(Collection.Date) %>%
+  summarise(Count = n()) %>%
+  ggplot(aes(x = Collection.Date, y = Count)) +
+  # ggplot(aes(x = 1:nrow(data), y = '')) # how to get x axis to be the total number of columns
   geom_line() +  # Add black outline
   geom_point() + 
-  labs(x = "Times sampled", y = "Average genera") +
+  labs(x = "Date", y = "Samples") +
   guides(fill = "none") +
   theme_classic()
 plot1
 ggsave(filename = "plot1.jpeg", plot = plot1, height = 5, width = 7, units = "in")
+
+data <- data %>%
+  mutate(Genus = ifelse(Genus == "", paste0("unidentified_", Family), Genus))
+
+# YEaAAHAHAHH FIGURED THIS OUT
+to_rarefy <- data %>%
+  group_by(Collection.Date) %>%
+  summarise(individuals = n(), 
+            num_of_genera = n_distinct(Genus))
+
+# Print the summarized result
+print(to_rarefy)
+  
+  
+
 
 
 
